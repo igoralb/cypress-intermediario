4dcbc5e8e3b0d8e47bf6aec4e8146b4be2cd3067
Igor Bastos Albuquerque <igor.bastos@live.com>
2025-12-12 13:35:44 -0300

    refact: melhora verificação de login

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -1,16 +1,32 @@
 Cypress.Commands.add('login', (
-    user = Cypress.env('user_name'),
-    password = Cypress.env('user_password'),
+  user = Cypress.env('user_name'),
+  password = Cypress.env('user_password'),
+  { cacheSession = true } = {},
 ) => {
-    const login = () => {
-        cy.visit('/users/sign_in')
+  const login = () => {
+    cy.visit('/users/sign_in')
 
-        cy.get("[data-qa-selector='login_field']").type(user)
-        cy.get("[data-qa-selector='password_field']").type(password, { log: false })
-        cy.get("[data-qa-selector='sign_in_button']").click()
-    }
+    cy.get("[data-qa-selector='login_field']").type(user)
+    cy.get("[data-qa-selector='password_field']").type(password, { log: false })
+    cy.get("[data-qa-selector='sign_in_button']").click()
+  }
 
+  const validate = () => {
+    cy.visit('/')
+    cy.location('pathname', { timeout: 1000 })
+      .should('not.eq', '/users/sign_in')
+  }
+
+  const options = {
+    cacheAcrossSpecs: true,
+    validate,
+  }
+
+  if (cacheSession) {
+    cy.session(user, login, options)
+  } else {
     login()
+  }
 })
 
 Cypress.Commands.add('logout', () => {
@@ -19,9 +35,9 @@ Cypress.Commands.add('logout', () => {
 
 })
 
-Cypress.Commands.add('gui_createProject', project =>{
+Cypress.Commands.add('gui_createProject', project => {
     cy.visit('/projects/new')
-
+    
     cy.get('#project_name').type(project.name)
     cy.get('#project_description').type(project.description)
     cy.get('.qa-initialize-with-readme-checkbox').check()
